9ad78ce827417434b6207cefa3774a6ebe3d5d21
josseth <jossethale32@hotmail.com>
2022-07-05 20:49:02 -0600

    Agregar archivos de proyecto.

---
+++ testswipe/testswipe/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace testswipe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SwipeItem_Invoked(object sender, EventArgs e)
+        {
+            await DisplayAlert("INFO", "AGREGADO A FAVORITOS", "OK");
+        }
+
+        private async void SwipeItem_Invoked_1(object sender, EventArgs e)
+        {
+            await DisplayAlert("INFO", "ELIMINADO", "OK");
+            swipe1.IsVisible = false;
+        }
+    }
+}
